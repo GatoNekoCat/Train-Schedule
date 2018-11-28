d69206dc4ebc0f8bd61e42e21ae11435546e17f1
Raiicon <orankai@gmail.com>
2018-11-27 23:37:40 -0500

    hooked into moment. Table is reading data from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,24 +5,43 @@ $(document).ready(function(){
 
     //  On click event for form submission to firebase
     $('#add-train').on("click", function(event){
+        // prevents the form from refreshing the page
         event.preventDefault();  
+        // Get input for name, destination, frequency and the start time
         var trainName = $('#trainName').val().trim();
         var destination = $('#destination').val().trim();
         var frequency = $('#frequency').val().trim();
-        var firstTrainTime = $('#firstTrainTime').val().trim();
-        
-  
-  
-  
-  
+        var startTime = $('#start-time').val().trim();  
+//   
+        // push our data into the database
         database.ref().push({
+        // data properties
         name : trainName,
         destination : destination ,
         frequency : frequency,
-        firstTrainTime : firstTrainTime
-            
+        startTime : startTime            
         });
     }); 
+
+    // on a firebase update this will add the data to the table. 
+    database.ref().on("child_added", function(childSnapshot){
+
+        // read the properties of the snapshot
+        var name = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var frequency = childSnapshot.val().frequency;
+        var startTime = childSnapshot.val().startTime;
+
+         
+   
+   
+             $('.table').append('<tr><td>' + name +
+                 '</td> <td>' + destination +
+                 '</td> <td>' + frequency +
+                 '</td> <td>' + startTime +                 
+                 ' </td> </tr>');
+         }) 
+   
 
 
 })
